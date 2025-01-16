da918ab3cf280f32fc31d078feace90d4b57c9bb
Genta Mateo Kamekawa Roa <gentaroa1@gmail.com>
2025-01-16 14:12:29 +0100

    feat: implementar la clase Product
    - Añadir la implementación de la clase Product con atributos como id, name, description, price, feature y url_image.
    - Incluir anotaciones de JPA para la persistencia en la base de datos.
    - Utilizar Lombok para la generación automática de getters, setters y constructores.

--- src/main/java/com/factoria/proyecto_final_ecom_fs/model/Product.java
+++ src/main/java/com/factoria/proyecto_final_ecom_fs/model/Product.java
@@ -1,4 +1,34 @@
 package com.factoria.proyecto_final_ecom_fs.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique=true, nullable=false)
+    private String name;
+
+    @Column(length=400, nullable=false)
+    private String description;
+
+    @Column(nullable=false)
+    private float price;
+
+    @Column(nullable=false)
+    private boolean feature;
+
+    @Column(unique=true, nullable=false)
+    private String url_image;
 }
